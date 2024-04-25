2cf5db8c6d4266310abe3929887897912ff04111
julamie <59706363+julamie@users.noreply.github.com>
2024-04-25 21:34:48 +0200

    Added test cases

--- src/test/java/Logic/WordTest.java
+++ src/test/java/Logic/WordTest.java
@@ -25,20 +25,49 @@ class WordTest {
     }
 
     @Test
-    void getLetter() {
+    void lastLetterShouldBeO() {
         assertEquals('O', testWordHorizontally.getLetter(3));
         assertEquals('O', testWordVertically.getLetter(3));
     }
 
     @Test
-    void getLetterValue() {
+    void lastLetterValueShouldBeTwo() {
         assertEquals(2, testWordHorizontally.getLetterValue(3));
         assertEquals(2, testWordVertically.getLetterValue(3));
     }
 
     @Test
-    void getCoordinate() {
+    void getCoordinatesShouldReturnCorrectCoordinateForLastLetter() {
         assertEquals(new Coordinates(4, 8), testWordHorizontally.getCoordinates(3));
         assertEquals(new Coordinates(7, 5), testWordVertically.getCoordinates(3));
+    }
+
+    @Test
+    void getLetterAtCoordinatesShouldFindFirstPosition() {
+        assertEquals('A', testWordHorizontally.getLetterAtCoordinates(new Coordinates(4, 5)));
+    }
+
+    @Test
+    void getLetterAtCoordinatesShouldReturnLetterAtPosition() {
+        assertEquals('O', testWordHorizontally.getLetterAtCoordinates(new Coordinates(4, 8)));
+        assertEquals('O', testWordVertically.getLetterAtCoordinates(new Coordinates(7, 5)));
+    }
+
+    @Test
+    void getLetterAtCoordinatesShouldReturnNullForWrongCoordinates() {
+        assertNull(testWordHorizontally.getLetterAtCoordinates(new Coordinates(0,0)));
+        assertNull(testWordVertically.getLetterAtCoordinates(new Coordinates(0,0)));
+
+        assertNull(testWordHorizontally.getLetterAtCoordinates(new Coordinates(4,9)));
+        assertNull(testWordVertically.getLetterAtCoordinates(new Coordinates(8,5)));
+
+        assertNull(testWordHorizontally.getLetterAtCoordinates(new Coordinates(4,4)));
+        assertNull(testWordVertically.getLetterAtCoordinates(new Coordinates(3,5)));
+    }
+
+    @Test
+    void toStringShouldReturnCorrectWord() {
+        assertEquals("AUTO", testWordHorizontally.toString());
+        assertEquals("AUTO", testWordVertically.toString());
     }
 }
