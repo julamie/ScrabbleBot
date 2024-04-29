ef4bc2d17e652f9664148d2af53d2e9149cb85cf
julamie <59706363+julamie@users.noreply.github.com>
2024-04-30 01:44:00 +0200

    Added a class responsible for calculating how many points a play makes

---
+++ src/main/java/logic/Scoring.java
@@ -0,0 +1,68 @@
+package logic;
+
+import boardStructure.Board;
+import boardStructure.Square;
+
+public class Scoring {
+
+    private final Board board;
+    private final Word word;
+
+    public Scoring(Board board, Word word) {
+        this.board = board;
+        this.word = word;
+    }
+
+    public int calculateWordScore() {
+        int wordScore = 0;
+        int wordMultiplicator = 1;
+
+        for (int i = 0; i < this.word.getLength(); i++) {
+            int letterValue = this.word.getLetterValue(i);
+            Square square = this.board.getSquareAt(word.getCoordinates(i));
+
+            // calculate no extra points for tiles that are already there
+            if (square.isOccupied()) {
+                wordScore += square.getValue();
+                continue;
+            }
+
+            switch (square.getType()) {
+                case NORMAL -> wordScore += letterValue;
+                case LETTER_BONUS_DOUBLE -> wordScore += 2 * letterValue;
+                case LETTER_BONUS_TRIPLE -> wordScore += 3 * letterValue;
+                case WORD_BONUS_DOUBLE -> {
+                    wordScore += letterValue;
+                    wordMultiplicator *= 2;
+                }
+                case WORD_BONUS_TRIPLE -> {
+                    wordScore += letterValue;
+                    wordMultiplicator *= 3;
+                }
+            }
+        }
+
+        return wordMultiplicator * wordScore;
+    }
+
+    public int calculateCrossWordScore(Word crossWord) {
+        int crossWordScore = 0;
+        for (int i = 0; i < crossWord.getLength(); i++) {
+            crossWordScore += crossWord.getLetterValue(i);
+        }
+
+        return crossWordScore;
+    }
+
+    public int calculateScore() {
+        int totalScore = 0;
+        totalScore += calculateWordScore();
+
+        Word[] crossWords = new CrossCheck(this.board, this.word).getCrossWordList();
+        for (Word crossWord: crossWords) {
+            totalScore += calculateCrossWordScore(crossWord);
+        }
+
+        return totalScore;
+    }
+}
