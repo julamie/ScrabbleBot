2bcdc797f386c28cc32b79274ecca2f6dfa387f0
julamie <59706363+julamie@users.noreply.github.com>
2024-05-07 01:19:32 +0200

    Added English as new language with new letter distribution and values

--- src/main/java/gameSetup/Language.java
+++ src/main/java/gameSetup/Language.java
@@ -3,7 +3,8 @@ package gameSetup;
 import java.util.HashMap;
 
 public enum Language {
-    GERMAN (generateGermanLetterDistribution(), generateGermanLetterValues());
+    GERMAN (generateGermanLetterDistribution(), generateGermanLetterValues()),
+    ENGLISH (generateEnglishLetterDistribution(), generateEnglishLetterValues());
 
     private final HashMap<Character, Integer> letterDistribution;
     private final HashMap<Character, Integer> letterValues;
@@ -17,10 +18,10 @@ public enum Language {
         HashMap<Character, Integer> distribution = new HashMap<>();
 
         // distribution based on the German Scrabble version
+        distribution.put('E', 15);
         distribution.put('N', 9);
         distribution.put('S', 7);
         distribution.put('I', 6);
-        distribution.put('E', 15);
         distribution.put('R', 6);
         distribution.put('T', 6);
         distribution.put('U', 6);
@@ -46,6 +47,40 @@ public enum Language {
         distribution.put('X', 1);
         distribution.put('Q', 1);
         distribution.put('Y', 1);
+        distribution.put('?', 2);
+
+        return distribution;
+    }
+    private static HashMap<Character, Integer> generateEnglishLetterDistribution() {
+        HashMap<Character, Integer> distribution = new HashMap<>();
+
+        // distribution based on the English Scrabble version
+        distribution.put('E', 12);
+        distribution.put('A', 9);
+        distribution.put('I', 9);
+        distribution.put('O', 8);
+        distribution.put('N', 6);
+        distribution.put('R', 6);
+        distribution.put('T', 6);
+        distribution.put('L', 4);
+        distribution.put('S', 4);
+        distribution.put('U', 4);
+        distribution.put('D', 4);
+        distribution.put('G', 3);
+        distribution.put('B', 2);
+        distribution.put('C', 2);
+        distribution.put('M', 2);
+        distribution.put('P', 2);
+        distribution.put('F', 2);
+        distribution.put('H', 2);
+        distribution.put('V', 2);
+        distribution.put('W', 2);
+        distribution.put('Y', 2);
+        distribution.put('K', 1);
+        distribution.put('J', 1);
+        distribution.put('X', 1);
+        distribution.put('Q', 1);
+        distribution.put('Z', 1);
         distribution.put('?', 2);
 
         return distribution;
@@ -90,6 +125,48 @@ public enum Language {
 
         letterValues.put('Q', 10);
         letterValues.put('Y', 10);
+
+        letterValues.put('?', 0);
+
+        return letterValues;
+    }
+
+    private static HashMap<Character, Integer> generateEnglishLetterValues() {
+        HashMap<Character, Integer> letterValues = new HashMap<>();
+
+        // letter values based on the English Scrabble version
+        letterValues.put('E', 1);
+        letterValues.put('A', 1);
+        letterValues.put('I', 1);
+        letterValues.put('O', 1);
+        letterValues.put('N', 1);
+        letterValues.put('R', 1);
+        letterValues.put('T', 1);
+        letterValues.put('L', 1);
+        letterValues.put('S', 1);
+        letterValues.put('U', 1);
+
+        letterValues.put('D', 2);
+        letterValues.put('G', 2);
+
+        letterValues.put('B', 3);
+        letterValues.put('C', 3);
+        letterValues.put('M', 3);
+        letterValues.put('P', 3);
+
+        letterValues.put('F', 4);
+        letterValues.put('H', 4);
+        letterValues.put('V', 4);
+        letterValues.put('W', 4);
+        letterValues.put('Y', 4);
+
+        letterValues.put('K', 5);
+
+        letterValues.put('J', 8);
+        letterValues.put('X', 8);
+
+        letterValues.put('Q', 10);
+        letterValues.put('Z', 10);
 
         letterValues.put('?', 0);
 
